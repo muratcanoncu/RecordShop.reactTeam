3264b661514cc3c5fcbd36edc357c3f04dd5276d
muratcanoncu <murat.oncu1992@hotmail.com>
2021-03-23 10:09:28 +0100

    minor styling

--- myapp/src/Components/AlbumPage/AlbumTemplateHero.js
+++ myapp/src/Components/AlbumPage/AlbumTemplateHero.js
@@ -13,24 +13,35 @@ const AlbumTemplateHero = (props) => {
   return (
     <Row className="m-3 pb-4" style={{ lineHeight: "1" }}>
       <Col className="d-flex flex-align-items-center justify-content-center">
-        <div className="mt-3">
+        <div className="mt-3 w-100 text-center">
           <h1>{title}</h1>
           <p>
             {artist}, {releaseYear} {genre}
           </p>
 
           <p>Price: {price} €</p>
-          {props.albumData.trackList.map((track, index) => {
-            return (
-              <>
-                <tr key={index}>
-                  <td>{track.no}</td>
-                  <td>{track.title}</td>
-                  <td>{track.length}</td>
-                </tr>
-              </>
-            );
-          })}
+          <table className="w-100 mt-4">
+            <thead style={{ borderBottom: "1px solid #ff7700" }}>
+              <tr className="text-left ">
+                <th>#</th>
+                <th>Name</th>
+                <th>Duration</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.albumData.trackList.map((track, index) => {
+                return (
+                  <>
+                    <tr key={track.id} className="text-left">
+                      <td className="py-1">{track.no}</td>
+                      <td>{track.title}</td>
+                      <td>{track.length}</td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </Col>
       <Col>
