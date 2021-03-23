e495ed45bc73e91a6e629a57ae08c38213c1dca0
muratcanoncu <murat.oncu1992@hotmail.com>
2021-03-23 10:50:56 +0100

    path changed 2

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -31,7 +31,7 @@ function App() {
         </div>
 
         <Switch>
-          <Route path="/" exact>
+          <Route path="/RecordShop.reactTeam" exact>
             <Carousel />
             {context.myStateData.popUp ? <PopUp></PopUp> : null}
             <MainAlbumContainer />
--- myapp/src/Components/Navbar/MyNavbar.js
+++ myapp/src/Components/Navbar/MyNavbar.js
@@ -21,7 +21,10 @@ function MyNavbar() {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" variant="dark">
-        <Navbar.Brand href="/" className="d-flex align-items-center ml-3">
+        <Navbar.Brand
+          href="/RecordShop.reactTeam"
+          className="d-flex align-items-center ml-3"
+        >
           <i
             className="fas fa-record-vinyl mr-3"
             style={{ fontSize: "3rem", color: "#353839" }}
@@ -44,7 +47,7 @@ function MyNavbar() {
             style={{ width: "400px" }}
           >
             <Nav.Link
-              href="/"
+              href="/RecordShop.reactTeam"
               className="font-weight-bolder"
               style={{ fontSize: "1.2rem" }}
             >
